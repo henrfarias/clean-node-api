689be61fe359e4817c2c0ee593e81d59c68a5902
Henrique Farias <henrique.biasibetti@gmail.com>
2022-03-02 21:35:11 -0300

    test: ensure SignUpController return 400 if password confirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -133,4 +133,21 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual<Error>(new ServerError())
   })
+
+  test('Should return 400 if passwordConfirmation fails', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual<Error>(
+      new InvalidParamError('passwordConfirmation')
+    )
+  })
 })
