4d5ff00a8ae460c1f506fef0ba9546a3db06c39b
Henrique Farias <henrique.biasibetti@gmail.com>
2022-03-03 11:10:55 -0300

    feat: ensure EmailValidatorAdapter return true if validator return true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,23 @@
 import { EmailValidatorAdapter } from './email-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true
+  }
+}))
 
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator return false', () => {
     const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValidEmail = sut.isValid('invalid_email@mail.com')
     expect(isValidEmail).toBeFalsy()
+  })
+
+  test('Should return true if validator return true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValidEmail = sut.isValid('valid_email@mail.com')
+    expect(isValidEmail).toBeTruthy()
   })
 })
